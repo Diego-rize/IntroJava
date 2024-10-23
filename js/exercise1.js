8f72f3a51d78cb39a276d00d163b7b011d8055db
Diego-rize <2302131@utrivieramaya.edu.mx>
2024-10-23 07:52:55 -0500

    first commit

---
+++ js/exercise1.js
@@ -0,0 +1,7 @@
+
+//los comentarios pueden hacer que el codigo sea legible
+
+//Este es mi primer codigo js
+
+/* los comentarios pueden hacer que el codigo sea legible,
+ Este es mi primer codigo js  */
---
+++ js/typeof.js
@@ -0,0 +1,5 @@
+console.log(typeof 'Dieguito') //string
+console.log(typeof 12)     //number
+console.log(typeof true)   //boolean
+console.log(typeof null)    //object type
+console.log(typeof undefined) //undefined
